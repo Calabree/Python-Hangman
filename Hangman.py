b5df682190c9e8a72b97571d70e0e23ba0165b4e
Peter Calabrese <peter@thirtyfour.tech>
2020-09-04 15:05:57 -0400

    added global counter for number of guesses left

--- Hangman.py
+++ Hangman.py
@@ -5,6 +5,7 @@ def newWord(word, wordToGuess, blankWordToGuess):
 
 def wordChecker(userGuess, wordToGuess, blankToGuess):
     x=0
+    global guessesLeft
     letterGuessedCorrectly=False
     for i in word:
         if i == userGuess:
@@ -14,14 +15,21 @@ def wordChecker(userGuess, wordToGuess, blankToGuess):
         x+=1
     if letterGuessedCorrectly == False:
         print(f'Looks like your guess {userGuess} was wrong')
-
-
-
+        guessesLeft = guessesLeft-1
+        print(f'You have {guessesLeft} guesses left! Choose carefully!')
+gameOn = True
+guessesLeft=5
 word = input ("Provide a word: ")
 wordToGuess= []
 blankToGuess=[]
 newWord(word, wordToGuess, blankToGuess)
 
-userGuess = input('Guess a letter: ')
-wordChecker(userGuess, wordToGuess, blankToGuess)
-print(*blankToGuess)
+while (gameOn ==  True):
+
+    userGuess = input('Guess a letter: ')
+    wordChecker(userGuess, wordToGuess, blankToGuess)
+    print(*blankToGuess)
+
+    if guessesLeft == 0:
+        print ("Game Over!")
+        gameOn = False
