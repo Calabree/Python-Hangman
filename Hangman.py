d89d3e073c1db5d1301be5effc87ad14ac741fa3
Peter Calabrese <peter@thirtyfour.tech>
2020-09-04 14:47:31 -0400

    fixed word checker, now replaces index in list with correctly guessed letter

--- Hangman.py
+++ Hangman.py
@@ -1,13 +1,25 @@
-def split(s):
-    return [char for char in s]
-    
-def newWord(word):
-    wordToGuess=split(word)
-    wordToGuess2=[]
-    for x in range(len(wordToGuess)):
-        wordToGuess2.append('_')
-    print(*wordToGuess2)
+def newWord(word, wordToGuess, blankWordToGuess):
+    wordToGuess=list(word)
+    for x in wordToGuess:
+        blankWordToGuess.append('_')
+
+def wordChecker(userGuess, wordToGuess, blankToGuess):
+    x=0
+    for i in word:
+        if i == userGuess:
+            print('found')
+            blankToGuess[x] = userGuess
+        else:
+            print(f'{userGuess} is not in this word!')
+        x+=1
+
 
 
 word = input ("Provide a word: ")
-newWord(word)
+wordToGuess= []
+blankToGuess=[]
+newWord(word, wordToGuess, blankToGuess)
+
+userGuess = input('Guess a letter: ')
+wordChecker(userGuess, wordToGuess, blankToGuess)
+print(*blankToGuess)
