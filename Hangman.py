b7349d8067336606299c5b9acee63dd59f10cc1f
Peter Calabrese <peter@thirtyfour.tech>
2020-09-05 17:54:09 -0400

    Added Hangman drawing and finalized main

--- Hangman.py
+++ Hangman.py
@@ -1,3 +1,5 @@
+from os import system, name
+
 def newWord(word, wordToGuess, blankWordToGuess):
     wordToGuess=list(word)
     for x in wordToGuess:
@@ -17,9 +19,91 @@ def wordChecker(userGuess, wordToGuess, blankToGuess):
         print(f'Looks like your guess {userGuess} was wrong')
         guessesLeft = guessesLeft-1
         print(f'You have {guessesLeft} guesses left! Choose carefully!')
+
+def theMan(guessesLeft):
+
+    switch={
+        6:'''
+        +---+
+        |   |
+            |
+            |
+            |
+            |
+        =========''',
+        5: '''
+        +---+
+        |   |
+        O   |
+            |
+            |
+            |
+        ======== ''', 
+        4: '''
+        +---+
+        |   |
+        O   |
+        |   |
+            |
+            |
+        ======== ''',
+        3: '''
+        +---+
+        |   |
+        O   |
+       /|   |
+            |
+            |
+        ======== ''',
+        2: '''
+        +---+
+        |   |
+        O   |
+       /|\  |
+            |
+            |
+        ======== ''',
+        1: '''
+        +---+
+        |   |
+        O   |
+       /|\  |
+       /    |
+            |
+        ======== ''', 
+        0: '''
+        +---+
+        |   |
+        O   |
+       /|\  |
+       / \  |
+            |
+        ======== '''
+    }
+    return switch.get(guessesLeft)
+
+def clear(): 
+  
+    if name == 'nt':
+        _ = system('cls') 
+    else: 
+        _ = system('clear') 
+
+def gameOver():
+    
+    global guessesLeft, word, gameOn
+    if guessesLeft == 0:
+        print ("Game Over!")
+        gameOn = False
+    if list(word) == blankToGuess:
+        print ("you won!")
+        gameOn = False
+    
+        
+clear()
 gameOn = True
-guessesLeft=5
-word = input ("Provide a word: ")
+guessesLeft=6
+word = input ("Provide a word: ").lower()
 wordToGuess= []
 blankToGuess=[]
 newWord(word, wordToGuess, blankToGuess)
@@ -29,7 +113,7 @@ while (gameOn ==  True):
     userGuess = input('Guess a letter: ')
     wordChecker(userGuess, wordToGuess, blankToGuess)
     print(*blankToGuess)
-
-    if guessesLeft == 0:
-        print ("Game Over!")
-        gameOn = False
+    print(theMan(guessesLeft))
+    gameOver()
+    wait=input("Press enter to continue")
+    clear()
